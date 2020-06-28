4c0136d6efe70593a0fa49176504f41290503e01
Jeremy BEAUCOUSIN <beaucousin.jeremy@valeuriad.fr>
2020-06-28 16:39:28 +0200

    Add first inputs interactions

--- src/FlappyHeron.ts
+++ src/FlappyHeron.ts
@@ -4,31 +4,91 @@ import background from './assets/background.png';
 import heron from './assets/heron.png';
 import ground from './assets/ground.png';
 
-
-export default class FlappyHeron extends Phaser.Scene
-{
-    constructor ()
-    {
+export default class FlappyHeron extends Phaser.Scene {
+    
+    player;
+    constructor() {
         super('FlappyHeron');
     }
 
-
-
-    preload ()
-    {
+    preload() {
         this.load.image('background', background);
-        this.load.image('heron', heron);
         this.load.image('ground', ground);
+
+        this.load.spritesheet('heron', heron, { frameWidth: 83, frameHeight: 64 });
     }
 
 
-    create ()
-    {
+    create() {
+        // Background image
         this.add.image(384, 448, 'background');
-  
-        let platforms = this.physics.add.staticGroup();
 
-        let starfield = this.add.tileSprite(384, 960, 768, 128, 'ground');
+        // Group of ground
+        let grounds = this.physics.add.staticGroup();
+
+        // Ground repeatable
+        let ground = this.add.tileSprite(384, 960, 768, 128, 'ground');
+
+        grounds.add(ground);
+
+        // Player
+        this.player = this.physics.add.sprite(384, 400, 'heron');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        // Colision with grounds
+        this.physics.add.collider(this.player, grounds);
+
+        // Animations
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('heron', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'heron', frame: 1 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('heron', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    
+    }
+
+    update() {
+        // Inputs
+        let cursors = this.input.keyboard.createCursorKeys();
+
+        // Left
+        if (cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+        }
+
+        // Right
+        else if (cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+        }
+
+        // Inactive
+        else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
+        }
+
+        // Up
+        if (cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 }
 
